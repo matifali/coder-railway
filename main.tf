a10bbae784190bf3c2f2aa84a49ab5ee1e5ded4d
Muhammad Atif Ali <me@matifali.dev>
2024-01-04 13:06:43 +0300

    Refactor code formatting in main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
-		coder = {
-			source = "coder/coder"
-		}
+    coder = {
+      source = "coder/coder"
+    }
     railway = {
-      source = "terraform-community-providers/railway"
+      source  = "terraform-community-providers/railway"
       version = "~> 0.2.0"
     }
   }
@@ -21,8 +21,8 @@ data "coder_provisioner" "me" {}
 data "coder_workspace" "me" {}
 
 resource "coder_agent" "main" {
-	arch = "amd64"
-	os = "linux"
+  arch = "amd64"
+  os   = "linux"
 
   metadata {
     display_name = "CPU Usage"
@@ -48,30 +48,31 @@ resource "railway_project" "coder_workspace" {
 }
 
 resource "railway_service" "workspace" {
-  count = data.coder_workspace.me.start_count
-	name = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
-	project_id = railway_project.coder_workspace.id
-	source_repo = "matifali/coder-railway"
-	root_directory = "build"
+  count          = data.coder_workspace.me.start_count
+  name           = "coder-${data.coder_workspace.me.owner}-${data.coder_workspace.me.name}"
+  project_id     = railway_project.coder_workspace.id
+  source_repo    = "matifali/coder-railway"
+  root_directory = "build"
+  config_path    = "railway.json"
 }
 
 resource "railway_variable" "coder_agent_init_script" {
-  count = data.coder_workspace.me.start_count
-	service_id = railway_service.workspace[0].id
-	environment_id = railway_project.coder_workspace.default_environment.id
-	name = "CODER_AGENT_INIT_SCRIPT"
-	value = base64encode(coder_agent.main.init_script)
+  count          = data.coder_workspace.me.start_count
+  service_id     = railway_service.workspace[0].id
+  environment_id = railway_project.coder_workspace.default_environment.id
+  name           = "CODER_AGENT_INIT_SCRIPT"
+  value          = base64encode(coder_agent.main.init_script)
 }
 
 resource "railway_variable" "coder_agent_token" {
-  count = data.coder_workspace.me.start_count
-	service_id = railway_service.workspace[0].id
-	environment_id = railway_project.coder_workspace.default_environment.id
-	name = "CODER_AGENT_TOKEN"
-	value = coder_agent.main.token
+  count          = data.coder_workspace.me.start_count
+  service_id     = railway_service.workspace[0].id
+  environment_id = railway_project.coder_workspace.default_environment.id
+  name           = "CODER_AGENT_TOKEN"
+  value          = coder_agent.main.token
 }
 
 module "code-server" {
-  source = "https://registry.coder.com/modules/code-server"
+  source   = "https://registry.coder.com/modules/code-server"
   agent_id = coder_agent.main.id
 }
